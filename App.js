3d22d6caba831dcd0b98ac224c48edaa425a93c9
Henrique Barbosa de Souza Jr <henrique@hbveterinaria.com.br>
2020-08-03 03:13:42 -0300

    Ponto inicial Aplicativo React Native consumo de API

--- App.js
+++ App.js
@@ -1,21 +1,46 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+const axios = require('axios');
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Consulta de Dados a uma API</Text>
       <StatusBar style="auto" />
+    <TouchableOpacity
+    style={styles.Button}
+    onPress={ function(){
+        axios({
+          method:'GET',
+          url:'http://18.188.214.149:3334/produtos',
+          }).then(res=>{
+              console.log(res.data);
+          }).catch(err => console.log(err));
+
+        }
+      }
+    >
+    <Text>Consultar</Text>
+    </TouchableOpacity>
     </View>
   );
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#1191',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  Button:{
+      backgroundColor:'#87CEFA',
+      height:45,
+      width:'80%',
+      margin:10,
+      justifyContent:'center',
+      alignItems:'center',
+      borderRadius:7
+
+  }
 });
